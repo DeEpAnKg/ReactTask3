fa5c72515164f37bd73f3dbc1f72e63c599608b0
DeEpAnKg <deepankgupta1453@gmail.com>
2023-10-18 13:21:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import './index.css'
 import './App.css';
-
+import react from 'react';
+import ColorItem from './color-item'
+// import color-item from './color-item'
 function App() {
+  const colors=['#f54254','#5af542','#5af542','#ddf542','#f542d1','#42f5dd','#f5a742','#a742f5','#b362ae','#62b372','#ab4211','#348573','#26050a','#c2a919'];
+  const setTheme=(color)=>{
+    document.documentElement.style.setProperty('--bg-color',color)
+  }
+  const setColor=(event)=>{
+    const currentColor = event.target.style.getPropertyValue('--bg-color')
+    setTheme(currentColor)
+    console.log(currentColor);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="color-switcher">
+        <h1 className="heading">Color picker</h1>
+        <div className="color-list">
+          {colors.map((color , idx)=> < ColorItem setColor={setColor} color={color}/>)}
+        </div>
+        {/* <h3 className='btn'>Pick a color</h3> */}
+      </div>
     </div>
   );
 }
